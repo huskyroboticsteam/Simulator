b9fd15b15d8d716d56da10f3f4464952c985465a
Connor Reinholdtsen <connorreinholdtsen@outlook.com>
2021-11-08 19:42:55 -0800

    Convert to websockets

--- Assets/Scripts/MissionControlListener.cs
+++ Assets/Scripts/MissionControlListener.cs
@@ -1,96 +1,68 @@
-﻿using UnityEngine;
-using System;
-using System.IO;
-using System.Net.Sockets;
-using System.Threading;
-using System.Text;
+﻿using System.Collections.Concurrent;
+using UnityEngine;
+using WebSocketSharp;
+using WebSocketSharp.Server;
 
 /// <summary>
-/// Connects to the Mission Control server and listens for requests.
+/// Connects to the Mission Control client and listens for requests.
 /// </summary>
 public class MissionControlListener : MonoBehaviour
 {
+    /// <summary>
+    /// This thread-safe queue stores requests made by the Mission Control
+    /// client. Requests are added to this queue in a WebSocketBehavior thread
+    /// so they can later be processed in the Unity main thread because Unity
+    /// API calls are not thread-safe.
+    /// </summary>
+    private static ConcurrentQueue<string> requests = new ConcurrentQueue<string>();
+
     public Rover rover;
 
-    private bool listening;
-    private TcpClient client;
-    private StreamReader reader;
+    private WebSocketServer server;
 
     private void OnEnable()
     {
-        // We must connect on a separate thread so that the simulator can
-        // continue running in parallel.
-        Thread thread = new Thread(Connect);
-        thread.Start();
+        StartServer();
+    }
+
+    /// <summary>
+    /// Start the simulator web socket server.
+    /// </summary>
+    private void StartServer()
+    {
+        server = new WebSocketServer("ws://localhost:3001");
+        server.AddWebSocketService<MissionControlService>("/");
+        server.KeepClean = false;
+        server.Start();
     }
 
     private void OnDisable()
     {
-        Disconnect();
+        StopServer();
     }
 
     /// <summary>
-    /// Creates a connection to the mission control server.
+    /// Stop the simulator web socket server.
     /// </summary>
-    private void Connect()
+    private void StopServer()
     {
-        try
+        server.Stop();
+    }
+
+    private void Update()
+    {
+        // Process requests enqueued form the WebSocketBehavior thread.
+        while (!requests.IsEmpty)
         {
-            client = new TcpClient("localhost", 3001);
+            string request;
+            requests.TryDequeue(out request);
+            ProcessRequest(request);
         }
-        catch (Exception)
-        {
-            Debug.Log("Connection to base station failed. Trying again in 3 seconds.");
-            Thread.Sleep(3000);
-            Connect();
-            return;
-        }
-
-        listening = true;
-        Debug.Log("Connected to base station");
-        reader = new StreamReader(client.GetStream());
-
-        StringBuilder jsonBuilder = new StringBuilder();
-        int openBraceCount = 0;
-        int firstBraceIndex = -1;
-
-        while (listening)
-        {
-            char ch = (char)reader.Read();
-            if (ch == '{')
-            {
-                if (firstBraceIndex == -1)
-                {
-                    firstBraceIndex = jsonBuilder.Length;
-                }
-                openBraceCount++;
-            }
-            else if (ch == '}')
-            {
-                openBraceCount--;
-            }
-
-            jsonBuilder.Append(ch);
-            if (firstBraceIndex != -1 && openBraceCount == 0)
-            {
-                // end of request
-                string request = jsonBuilder.ToString().Substring(firstBraceIndex);
-                ProcessRequest(request);
-
-                jsonBuilder = new StringBuilder();
-                firstBraceIndex = -1;
-            }
-        }
-
-        // Clean up.
-        reader.Close();
-        client.Close();
     }
 
     /// <summary>
-    /// Processes the given json request.
+    /// Processes the given json request received from Mission Control.
     /// </summary>
-    /// <param name="request">the json request in string format</param>
     private void ProcessRequest(string request)
     {
         int typeStartIndex = request.IndexOf("type") + 7;
@@ -118,10 +90,16 @@ public class MissionControlListener : MonoBehaviour
         }
     }
 
-    private void Disconnect()
+    /// <summary>
+    /// Provides websocket server behavior for processing data sent from
+    /// Mission Control.
+    /// </summary>
+    private class MissionControlService : WebSocketBehavior
     {
-        Debug.Log("Closing connection to Base Station");
-        listening = false;
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            requests.Enqueue(e.Data);
+        }
     }
 
     [System.Serializable]
