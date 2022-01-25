4a29961f7b9784208aadde47a4ce044ce4954d1d
Connor Reinholdtsen <connorreinholdtsen@outlook.com>
2022-01-24 17:08:44 -0800

    Add Gaussian noise to GPS Sensor

--- Assets/Scripts/GpsSensor.cs
+++ Assets/Scripts/GpsSensor.cs
@@ -4,8 +4,9 @@ using Newtonsoft.Json.Linq;
 
 /// <summary>
 /// A simulated GPS sensor on the rover which periodically sends the position
-/// of the simulated rover to the rover server. Assumes that the Unity origin
-/// maps to Null Island.
+/// of the simulated rover to the rover server. Applies Gaussian noise with
+/// a standard deviation of _noise meters to longitude and latitude. Assumes
+/// that the Unity origin maps to Null Island.
 /// </summary>
 public class GpsSensor : MonoBehaviour
 {
@@ -14,6 +15,11 @@ public class GpsSensor : MonoBehaviour
     /// </summary>
     private const double EarthRadius = 6_378_137.0;
 
+    /// <summary>
+    /// Standard deviation of Gaussian noise in meters.
+    /// </summary>
+    [SerializeField]
+    private float _noise;
     [SerializeField]
     private float _reportPeriod;
 
@@ -46,8 +52,8 @@ public class GpsSensor : MonoBehaviour
     private void ReportPosition()
     {
         // Use double precision since geographic degrees are very large.
-        double latitude = CartesianToGeographic(transform.position.z);
-        double longitude = CartesianToGeographic(transform.position.x);
+        double latitude = CartesianToGeographic(transform.position.z + _noise * GaussianRandom());
+        double longitude = CartesianToGeographic(transform.position.x + _noise * GaussianRandom());
 
         JObject positionReport = new JObject()
         {
@@ -56,6 +62,14 @@ public class GpsSensor : MonoBehaviour
             ["longitude"] = longitude
         };
         _socket.Send(positionReport);
+    }
+
+    /// <summary>
+    /// Returns a pseudorandom, Gaussian distributed value with mean 0 and standard deviation 1.
+    /// </summary>
+    private float GaussianRandom()
+    {
+        return Mathf.Sqrt(-2f * Mathf.Log(Random.value)) * Mathf.Sin(2f * Mathf.PI * Random.value);
     }
 
     private double CartesianToGeographic(float meters)
