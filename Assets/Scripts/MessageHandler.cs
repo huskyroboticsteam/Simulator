4b26e8dfd2dfeb37afee7ea357132bc49476189b
Kyle Reinholdtsen <kyler56@uw.edu>
2022-04-01 18:08:35 -0700

    Update MessageHandler to reflect JSON naming scheme

--- Assets/Scripts/MessageHandler.cs
+++ Assets/Scripts/MessageHandler.cs
@@ -50,9 +50,10 @@ public static class MessageHandler
         camera.StreamWidth = (int)cameraStreamOpenRequest["width"];
         camera.StreamHeight = (int)cameraStreamOpenRequest["height"];
         camera.IsStreaming = true;
-        if (cameraStreamOpenRequest.ContainsKey("intrinsicParams"))
+        if (cameraStreamOpenRequest.ContainsKey("intrinsicParameters"))
         {
-            camera.IntrinsicParams = ((JArray)cameraStreamOpenRequest["intrinsicParams"]).ToObject<float[]>(); ;
+            camera.IntrinsicParameters = ((JArray)cameraStreamOpenRequest["intrinsicParameters"])
+                .ToObject<float[]>();
         }
     }
 
