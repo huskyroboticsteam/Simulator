5a0822eeca74662986d178c9fbcd54d5376ac44c
Abhay D <a.deshpande012@gmail.com>
2023-09-22 21:59:21 -0700

    Fix arm conventions and initialization

--- Assets/Scripts/Utilities.cs
+++ Assets/Scripts/Utilities.cs
@@ -35,6 +35,25 @@ public static class Utilities
     }
 
     /// <summary>
+    /// Perform the swing-twist decomposition of a quaternion around a given axis.
+    /// This essentially decomposes a rotation into a rotation around the axis (the twist) and
+    /// a rotation around a vector perpendicular to that axis (the swing).
+    /// </summary>
+    /// <see cref="https://stackoverflow.com/a/22401169/6202029"/>
+    /// <param name="rotation">The rotation to decompose</param>
+    /// <param name="axis">The axis to perform the decomposition around</param>
+    /// <param name="swing">The rotation around a vector perpendicular to the given axis</param>
+    /// <param name="twist">The rotation around the given axis</param>
+    public static void SwingTwistDecomposition(Quaternion rotation, Vector3 axis, out Quaternion swing, out Quaternion twist) {
+        axis = axis.normalized;
+        Vector3 rotAxis = new Vector3(rotation.x, rotation.y, rotation.z);
+        Vector3 p = Vector3.Project(rotAxis, axis);
+        twist = new Quaternion(p.x, p.y, p.z, rotation.w);
+        twist.Normalize();
+        swing = rotation * Quaternion.Inverse(twist);
+    }
+
+    /// <summary>
     /// Returns a pseudorandom, Gaussian distributed value with mean 0 and
     /// standard deviation 1.
     /// </summary>
