b7b1a65f50a1ce95441cfdd8c28a93b7c807f832
PatrickRung <patrick.rung23@gmail.com>
2025-10-09 13:52:12 -0700

    add joint rotation

--- Assets/Scripts/ElbowMotor.cs
+++ Assets/Scripts/ElbowMotor.cs
@@ -1,18 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ElbowMotor : MonoBehaviour
+public class ElbowMotor : HingeMotor
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private GameObject backLinkageBeam;
+    [SerializeField]
+    private GameObject frontLinkageMountJoint;
+    private float initAngle = 0;
+    private float backLinkageCenterToPivot;
+    private float initHeight;
 
-    // Update is called once per frame
-    void Update()
+    protected override void Start()
     {
-        
+        base.Start();
+        initAngle = transform.localRotation.x;
+        backLinkageCenterToPivot = Mathf.Abs(Mathf.Sqrt(Mathf.Pow(backLinkageBeam.transform.localPosition.y, 2) +
+            Mathf.Pow(backLinkageBeam.transform.localPosition.z, 2)));
+        initHeight = backLinkageBeam.transform.localScale.z;
+    }
+    protected override void Render()
+    {
+        base.Render();
+        backLinkageBeam.transform.localPosition =
+            new Vector3(backLinkageBeam.transform.localPosition.x,
+                backLinkageBeam.transform.localPosition.y,
+                initHeight + Mathf.Abs(backLinkageCenterToPivot * Mathf.Sin(Mathf.Deg2Rad * transform.localRotation.x)));
+
+        frontLinkageMountJoint.transform.localRotation = new Quaternion(transform.localRotation.x,
+            frontLinkageMountJoint.transform.localRotation.y,
+            frontLinkageMountJoint.transform.localRotation.z,
+            frontLinkageMountJoint.transform.localRotation.w);
     }
 }
