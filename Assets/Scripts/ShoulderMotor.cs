f2a41f4ef1594d2f4e622892f80e4a29ecbd1bb2
PatrickRung <patrick.rung23@gmail.com>
2025-10-23 20:03:38 -0700

    new arm sim join meshes

--- Assets/Scripts/ShoulderMotor.cs
+++ Assets/Scripts/ShoulderMotor.cs
@@ -28,7 +28,9 @@ public class ShoulderMotor : HingeMotor
     private GameObject backLinkage;
     [SerializeField]
     private GameObject bottomLinkage;
+    [SerializeField]
     private float width = 0.205f;
+    [SerializeField]
     private float height = 0.609f;
 
     private float prevElbowAngle = 0;
@@ -36,17 +38,29 @@ public class ShoulderMotor : HingeMotor
     protected override void Start()
     {
         base.Start();
-        // width = Vector3.Distance(bottomFrontNode.transform.position,
-        //     bottomBackNode.transform.position);
-        Debug.Log(width);
-        // height = Vector3.Distance(bottomFrontNode.transform.position,
-        //     topBackNode.transform.position);
-        Debug.Log(height);
+        // Update mesh scale with inputed values
+        frontLinkageBeam.transform.localScale = new Vector3(frontLinkageBeam.transform.localScale.x,
+                                                            frontLinkageBeam.transform.localScale.y,
+                                                            height);
+        backLinkage.transform.localScale = new Vector3(backLinkage.transform.localScale.x,
+                                                            backLinkage.transform.localScale.y,
+                                                            height);
+        bottomLinkage.transform.localScale = new Vector3(bottomLinkage.transform.localScale.x,
+                                                            bottomLinkage.transform.localScale.y,
+                                                            width);
     }
     protected override void Render()
     {
         base.Render();
 
+        // No state change then don't update anything
+        if (prevElbowAngle == elbowMotor.transform.localEulerAngles.x &&
+                prevShoulderAngle == transform.localEulerAngles.x)
+        {
+            return;
+        }
+
+        // calculate the nodes of each of the joints of the linkage
         topFrontNode.transform.localPosition = new Vector3(topFrontNode.transform.localPosition.x,
             height * Mathf.Cos(elbowMotor.transform.localEulerAngles.x * Mathf.Deg2Rad),
             height * Mathf.Sin(elbowMotor.transform.localEulerAngles.x * Mathf.Deg2Rad));
@@ -59,7 +73,7 @@ public class ShoulderMotor : HingeMotor
             topFrontNode.transform.localPosition.y + (width * Mathf.Sin(transform.localEulerAngles.x * Mathf.Deg2Rad)),
             topFrontNode.transform.localPosition.z - (width * Mathf.Cos(transform.localEulerAngles.x * Mathf.Deg2Rad)));
 
-        // Update the forearm position
+        // Update the forearm position and rotation
         Vector3 topFrontToTopBack = topFrontNode.transform.localPosition - topBackNode.transform.localPosition;
         topFrontToTopBack = topFrontToTopBack / 2;
         frontLinkageBeam.transform.localPosition = topFrontNode.transform.localPosition - topFrontToTopBack;
@@ -69,7 +83,7 @@ public class ShoulderMotor : HingeMotor
             frontLinkageBeam.transform.localEulerAngles.z);
 
 
-        // Update the upper arm posiiton
+        // Update the upper arm posiiton and rotation
         Vector3 botFrontToTopFront = topFrontNode.transform.localPosition - bottomFrontNode.transform.localPosition;
         botFrontToTopFront = botFrontToTopFront / 2;
         frontLinkage.transform.localPosition = botFrontToTopFront + bottomFrontNode.transform.localPosition;
@@ -86,7 +100,7 @@ public class ShoulderMotor : HingeMotor
             frontLinkageBeam.transform.localEulerAngles.y,
             frontLinkageBeam.transform.localEulerAngles.z);
 
-        // Update bottom linkage
+        // Update bottom linkage position and rotation
         bottomLinkage.transform.localPosition = new Vector3(bottomBackNode.transform.localPosition.x,
             bottomFrontNode.transform.localPosition.y + ((width / 2) * Mathf.Sin(transform.localEulerAngles.x * Mathf.Deg2Rad)),
             bottomFrontNode.transform.localPosition.z - ((width / 2) * Mathf.Cos(transform.localEulerAngles.x * Mathf.Deg2Rad)));
@@ -96,7 +110,7 @@ public class ShoulderMotor : HingeMotor
             bottomLinkage.transform.localEulerAngles.z);
 
         MaxLimitPosition = localEulerAngleToEditorAngle(elbowMotor.transform.localEulerAngles.x + 60);
-        MinLimitPosition = localEulerAngleToEditorAngle(elbowMotor.transform.localEulerAngles.x - 60);
+        MinLimitPosition = Mathf.Max(localEulerAngleToEditorAngle(elbowMotor.transform.localEulerAngles.x - 60), -30);
 
         // Record the previous angle for checking whether state changed
         // Elbow and shoulder only rotate around x axis, no need to check any other angles
